2303f6b80d5ea45bc76c539e4bec3aa6e6cfb2a3
jeremieflrnt <55829284+jeremieflrnt@users.noreply.github.com>
2026-03-23 19:06:10 +0100

    calcator 0.1.3

--- Casks/calcator.rb
+++ Casks/calcator.rb
@@ -1,8 +1,8 @@
 cask "calcator" do
-  version "0.1.2"
-  sha256 "dedb29de4586197990674b0cb7a52d8a386df154997950545a8115ac896dd7b2"
+  version "0.1.3"
+  sha256 "735e5cf452b72240dda1fb19a3091806b2274dcceb5f0ff57f4eb5c9195809c3"
 
-  url "https://github.com/jeremieflrnt/calcator-release/releases/download/v#{version}/Calcator-#{version}.dmg"
+  url "https://github.com/jeremieflrnt/calcator-release/releases/download/v#{version}/Calcator.dmg"
   name "Calcator"
   desc "Calculator notebook app for macOS"
   homepage "https://calcator.app"
